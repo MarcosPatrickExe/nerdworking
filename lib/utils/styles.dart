d3d78d2ad312db9c853b6cd63a4ca0a7b7db0291
Marcos Patrick <marcospatrick039474@gmail.com>
2024-06-29 22:01:07 -0300

    adding the basic colors

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -6,11 +6,16 @@ enum AppColors{
 
   PURPLE_LIGHT( Color.fromARGB(255, 113, 3, 185) ),
   PURPLE_DARK( Color.fromARGB(255, 113, 3, 185) ),
-  WHITE( Color.fromARGB(255, 255, 255, 255) );
+  WHITE( Color.fromARGB(255, 255, 255, 255) ),
+  GRAY(  Color.fromARGB(255, 110, 120, 110)  ),
+  GRAY_TRANSPARENT(  Color.fromARGB(155, 110, 120, 110)  ),
+  TEXT_BLACK(  Color.fromARGB(180, 0, 0, 0)  ),
+  GREEN( Color.fromARGB(255, 23, 133, 2) ),
+  RED( Color.fromRGBO(234, 67, 53, 1.0) );
 
 
-  const AppColors( this.value );
-  final Color value;
+  const AppColors( this.color );
+  final Color color;
 }
 
 
