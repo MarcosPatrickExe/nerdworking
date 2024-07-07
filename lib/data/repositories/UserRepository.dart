680aac4b6af5826688c7141c330b45786a6aef7f
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-07-07 16:54:41 -0300

    Update UserRepository.dart

--- lib/data/repositories/UserRepository.dart
+++ lib/data/repositories/UserRepository.dart
@@ -8,7 +8,7 @@ class UserRepository extends UserDAO{
   Future<void> login( String email, String password ){
       return Future.value(false);
   }
-
+  
   Future<int> signup( String nickName, String avatar, String email, String password ){  throw Error(); }
 
   Future<bool> deleteAccountByID( int userID, String email ){ return Future.value(false);  }
