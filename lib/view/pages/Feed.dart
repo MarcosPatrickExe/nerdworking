d110899e5bbf68d71eb21ddba28d891e059c8a78
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-07-23 23:58:36 -0300

    adding the changes

--- lib/view/pages/Feed.dart
+++ lib/view/pages/Feed.dart
@@ -23,7 +23,7 @@ class FeedState extends State<Feed>{
       ),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric( horizontal: 40.0 ),
+          padding: const EdgeInsets.symmetric( horizontal: 40.0 ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
