311626202fb16c8e3300b0cbf4e2ecf843f143f2
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-12-21 21:29:55 -0300

    Update Field.dart

--- lib/domain/model/Field.dart
+++ lib/domain/model/Field.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class Field { 
     final String? labelText;
     final String? hintText;
-    final Icon? iconnn;
+    final Icon? iconnn; 
     final TextInputType? inputType; 
 
     const Field( this.labelText, this.hintText, this.iconnn, this.inputType );
