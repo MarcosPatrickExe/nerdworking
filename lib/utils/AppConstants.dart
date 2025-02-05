38442e9b0c733459ba4488d2478a0aee56837709
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-02-05 00:25:20 -0300

    Update AppConstants.dart

--- lib/utils/AppConstants.dart
+++ lib/utils/AppConstants.dart
@@ -1,7 +1,7 @@
 
 abstract final class AppConstants {
   
-  static const double heightButtonRatio = 0.055;
+  static const double heightButtonRatio = 0.055; 
   static const double widthButtonRatio = 0.341;
   
   
