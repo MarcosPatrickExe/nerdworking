1dfcc8561b6661cb38e619cab3e8410302a22756
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-02-08 00:32:13 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart'; 
 import 'package:nerdworking/utils/Styles.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart'; 
 import './view/pages/Login.dart';  
 import 'theme/AppThemeProvider.dart'; 
 
