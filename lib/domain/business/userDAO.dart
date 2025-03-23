2e7dffe4612418db4467921ed6b3b37128925f40
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-03-23 18:05:43 -0300

    Update userDAO.dart

--- lib/domain/business/userDAO.dart
+++ lib/domain/business/userDAO.dart
@@ -7,7 +7,7 @@ abstract class UserDAO{
   Future<void> login( String email, String password ); 
 
   Future<int> signup( String nickName, String avatar, String email, String password ); 
-
+ 
   Future<bool> deleteAccountByID( int userID, String email ); 
  
   Future<bool> updateAccountDataByID( int userID, User user ); 
