08db34debbd6945ca4c1123c280a6e7ccfcb823f
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-05-12 23:04:04 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'theme/AppThemeProvider.dart';
 
 void main() { 
   runApp( ChangeNotifierProvider(
+    
       create: (_)=> AppThemeProvider(),  
       child: const SettingAppTheme(), 
   ) );
