9fc9a98c6c49ebb5fd49773dd3f32f061a4c4bf4
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-05-28 23:25:56 -0300

    Update UserTypesDialog.dart

--- lib/view/components/UserTypesDialog.dart
+++ lib/view/components/UserTypesDialog.dart
@@ -4,7 +4,6 @@ import 'package:nerdworking/view/pages/CreateUserAccount.dart';
 
 class UserTypesDialog extends StatefulWidget{
 
-  
   BuildContext _parentContext;
   CreateUserAccountState _parentWidget;
 
